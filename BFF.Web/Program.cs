8e1b8af18b563693baa9690b24ead1e0f8656f6d
Héritier MAVINGA <heritier.mavinga@bluesoft-group.com>
2024-05-03 20:07:19 +0200

    #20240503: development api gateway with ocelot

--- BFF.Web/Program.cs
+++ BFF.Web/Program.cs
@@ -1,3 +1,7 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Cache.CacheManager;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//
+builder.Configuration
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot()
+    .AddDelegatingHandler<RequestLogger>()
+    .AddCacheManager(x => {
+        x.WithDictionaryHandle();
+    });
 
 var app = builder.Build();
 
@@ -20,4 +33,25 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//Ocelot
+await app.UseOcelot();
+
 app.Run();
+
+#region Extensions
+public class RequestLogger : DelegatingHandler
+{
+    private readonly ILogger<RequestLogger> _logger;
+
+    public RequestLogger(ILogger<RequestLogger> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Request [{request.Method}] - {request.RequestUri?.PathAndQuery} : {request.RequestUri?.Port}");
+        return base.SendAsync(request, cancellationToken);
+    }
+}
+#endregion
